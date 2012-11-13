7e07cc236234afacbbfa176c9f73b8f7f0899e28
Johnny Halife <johnny@mural.ly>
2012-11-13 20:27:00 -0300

    update first version

--- server.js
+++ server.js
@@ -4,5 +4,15 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get("/", function(req, res){
+	return res.sendfile(__dirname + "/public/index.html");
+});
+
+app.post("/operations", function(req, res){
+	var operations = req.body;
+
+	res.end();
+});
+
 
 app.listen(process.env.PORT || 5000);

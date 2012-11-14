bafde46f5f8010fb066b991f8a0e94326221e202
Johnny Halife <johnny@mural.ly>
2012-11-13 18:14:58 -0800

    added log streamer

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var app = express();
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
 
+app.configure('production', function(){
+	app.use(require('azure-logstreamer'));
+});
+
 app.post("/operations", function(req, res) {
 	console.log("received operations");
 	res.end();
